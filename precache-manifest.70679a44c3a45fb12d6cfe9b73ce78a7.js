fef74c1ed7bf78c7647bb1b7fa4872f36af6a277
KirillProgMaster <kirill.zarya@inbox.ru>
2021-08-20 22:23:46 +0300

    Updates

---
+++ precache-manifest.70679a44c3a45fb12d6cfe9b73ce78a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90add87edfce90415978cb6c0e7c4298",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "f0b9ad3175aeb5d5a454",
+    "url": "/Todolist/static/css/main.63885e3c.chunk.css"
+  },
+  {
+    "revision": "e2c5b66dc530bce671a5",
+    "url": "/Todolist/static/js/2.47c27b76.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist/static/js/2.47c27b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b9ad3175aeb5d5a454",
+    "url": "/Todolist/static/js/main.9421a441.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
