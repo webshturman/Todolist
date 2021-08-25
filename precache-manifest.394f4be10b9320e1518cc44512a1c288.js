7ce8165e51720f9648e5ca7495b4efa71b34dc6d
KirillProgMaster <kirill.zarya@inbox.ru>
2021-08-25 22:15:57 +0300

    Updates

---
+++ precache-manifest.394f4be10b9320e1518cc44512a1c288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b840423e6cc8beec1c3d1db645214a2",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "520cabde42292d63f3b4",
+    "url": "/Todolist/static/css/main.3981083a.chunk.css"
+  },
+  {
+    "revision": "a806935d5f30680ca0bf",
+    "url": "/Todolist/static/js/2.6e9e41fd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist/static/js/2.6e9e41fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520cabde42292d63f3b4",
+    "url": "/Todolist/static/js/main.c273f027.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Todolist/precache-manifest.0c58c356f512ef8cfa1bd177f8ebaaf5.js"
+  "/Todolist/precache-manifest.394f4be10b9320e1518cc44512a1c288.js"
 );
 
 self.addEventListener('message', (event) => {
